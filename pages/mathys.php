8189e267c07eeb7e9cc4040fbe1c7b93c9d51d66
Mathys Schmitter <mathys.schmitter@etudiant.univ-reims.fr>
2023-09-20 11:36:09 +0200

    page modifier

--- pages/mathys.php
+++ pages/mathys.php
@@ -18,7 +18,9 @@ $html = <<<HTML
         <p>MATHYS</p>
     </div>
     <div id="photo">
-        <img src="../assets/images/fond.png">
+        <img src="../assets/images/mathys/Dofus_Logo.png">
+        <img src="../assets/images/mathys/inconnu.png" width=15% height=15%>
+        <img src="../assets/images/mathys/Minecraft-Logo.png">
     </div>
     <div id="description">
         <p>Pour la petite histoire, je suis une lycéenne de 17 ans tout pile, en terminale...une bonne élève, c'est à dire que je caracole en tête de classe et, ayant la chance d'être désormais dans un bon lycée, je m'y sens suffisamment...bien n'est pas le mot, mais suffisamment en sécurité pour ne pas avoir envie de régresser. Car ça m'est arrivé, au collège...je pense que je vais essayer de vous expliquer un peu mon parcours. C'est encore la présentation la plus efficace!
@@ -26,6 +28,7 @@ $html = <<<HTML
             Comme pas mal d'enfants, j'ai commencé l'école tranquillement, en petite section, moyenne section...c'était long, en fait, ma grande hantise c'était la sieste de l'après-midi! Mais bon, dans ces classes là, on a toujours quelque chose à faire, et j'avais des amis, un "groupe" autour de moi. En fin de moyenne section, branle-bas de combat, me voilà diagnostiquée "zèbre"...bon, à cet âge là, ça ne voulait rien dire. Ensuite j'ai "sauté" la grande section. En CP, j'ai eu un très bon professeur et j'ai pris goût à ma position de tête de classe...je levais la main, questionnais presque tout le temps, mais elle avait réussi à me "canaliser", en sorte, en m'enjoignant de laisser parler les autres, ce que je faisais, avec plaisir. Si bien qu'au final, je n'étais pas trop casse-pieds...En CE1, pas d'histoires non plus, un professeur amusant et intéressant.
             </p>
     </div>
+    <img id="ballon" src="../assets/images/mathys/ballon_de_basket.png">
 </body>
 HTML;
 

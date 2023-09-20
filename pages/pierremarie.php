3e5b07ac5b5a22a63d2d87f739d7c339c6e6539e
Pierre-Marie <talbi.pierremarie@gmail.com>
2023-09-20 12:33:54 +0200

    test

--- pages/pierremarie.php
+++ pages/pierremarie.php
@@ -1,18 +1,32 @@
 <!DOCTYPE html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Teko">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/css/pierremarie.css">
 
     <title>Base</title>
 </head>
+
 <body>
-    <div>
-        Hello World !
+    <?php
+    include("component/header.php");
+    include("component/navbar.php");
+    ?>
+    <div id="titre">
+        Pierre-Marie
     </div>
-    <img src="">
-    <div>
-        Bonjour
+    <div id="img">
+        <img src="/assets/images/PM/Unknown.png">
+        <img src="/assets/images/PM/Honkai-Star-Rail.png">
     </div>
-</body> 
+    <div id="desc">
+        <p>Ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem
+            aperiam,<br>
+            Ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+    </div>
+</body>

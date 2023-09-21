1f32896ebc911c8d20e9520b8159991627b11bb1
Harmdhast <pandemonium.harmdhast@gmail.com>
2023-09-21 10:21:08 +0200

    gino

--- pages/gino.php
+++ pages/gino.php
@@ -1,29 +1,29 @@
 <!DOCTYPE html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="../assets/css/gino.css">
+    <link rel="stylesheet" href="/assets/css/gino.css">
 
     <title>Base</title>
 </head>
 
 <?php
     include("component/header.php");
-    include("component/navbar.php");
+
   ?>
 
-<body>
-    <div class="h1-div">
-        <h1>Gino</h1>
-    </div>
-<<<<<<< HEAD
-    <img loading="lazy" src="../assets/images/DSC_0922 2.jpg" alt="mwa">
+<body class="text-white">
+    <div class="container-fluid d-flex justifiy-content-center flex-column align-items-center">
+        <div class="h1-div">
+            <h1>Gino</h1>
+        </div>
+        <img loading="lazy" width="300px" height="300px" src="/assets/images/gino.png" alt="mwa">
 
-    <h1>Jeux vidéo Joué :</h1>
-    <p>MINECRAFT (et encore ça fait longtemps, et quand je l'ai lancé la dernière fois cetait pour faire des commands block..)<p>
-    
-=======
-    <img loading="lazy" src="/assets/images/gino.png" alt="mwa">
->>>>>>> refs/remotes/origin/main
+        <h1>Jeux vidéo Joué :</h1>
+        <p>MINECRAFT (et encore ça fait longtemps, et quand je l'ai lancé la dernière fois cetait pour faire des
+            commands block..)
+        <p>
+    </div>
 </body>
